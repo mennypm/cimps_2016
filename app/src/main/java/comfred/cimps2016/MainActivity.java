cf5dc800b7cdf7bbf1ba79c6833625668f1bcaa6
Freddy Íñiguez López <freddyiniguez@Mac-mini-de-cimat.local>
2016-09-12 15:33:10 -0500

    Launcher and QR code reader added.

---
+++ app/src/main/java/comfred/cimps2016/MainActivity.java
@@ -0,0 +1,41 @@
+package comfred.cimps2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.view.View;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scanQR(View v){
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        // Take the user ID and make a request to the database server
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        // Resume scanning.
+        // Uncomment this line to scan again.
+        // mScannerView.resumeCameraPreview(this);
+    }
+}
